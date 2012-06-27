527e602c53d9530ce9383f104b22e67ac3bc1793
denizhan <basaryavuz38@hotmail.com>
2012-06-27 12:59:15 +0300

    java script olarak kayıt ettim.

---
+++ nerede ne yenir(notepad++).js
@@ -0,0 +1,194 @@
+Ýstanbul'da nerde ne yenir??
+ 
+ 
+AVRUPA  YAKASI 
+
+Beyoðlu  (Taksim, Ýstiklal Caddesi, Tepebaþý, Cihangir, Galata, Tünel, Karaköy  Çevreleri) 
+
+Taksim /  Changa [Lüks] .......... Tahin ve Miso  Soslu Fýrýnda Patlýcan
+Cihangir/Doða Balýk  [Balýkçý]...... ......... . Palamut  Pilaki
+Cihangir / Savoy Pastanesi [Pastane]... .....Milföy Pasta 
+Ýstiklal Caddesi/  Kallavi (Meyhane)  ...........Çerkez  tavuðu 
+Ýstiklal Caddesi / Ýnci Pastanesi [Pastane]  ....Profiterol   
+Ýstiklal Caddesi/Hacý Baba Lokantasý [Türk Mutfaðý)  ....... Kuzu  Tandýr 
+Hacý Abdullah Lokantasý (Esnaf Lokantasý] .....  Kuzu Ýncik  Patlýcanlý
+Ýstiklal Caddesi/Hacý Bekir  (Tatlýcý)..... ......... .... özel öneri  yok  
+Tarlabaþý/Lale Ýþkembecisi [Çorbacý]  ............ .....Ýþkembe  çorbasý 
+Nevizade / Boncuk Restoran (Meyhane)... .........  Uskumru Dolmasý
+Nevizade /  imroz (Meyhane) ............ ......... ......... Soslu Torik 
+Balýkpazan /  Cumhuriyet [Meyhane)... ......... .........Fava 
+Balýkpazarý/ Nizam Pide  [Pideci].... ......... ......... ....Spesiyal  Pide
+Galatasaray/ Fýccýn  (Etnik]..... ......... ......... ......... ....Fýccýn ve  Gebýn 
+Galatasaray/ Výctor Levi  [Meyhane]... ......... ......... ..Special 
+Galatasaray/ Pano Sarapem  (Meyhane]... ......... ......Special 
+Asmalýmescýt / Refik Restoran  (Meyhane)... ......... ..Arnavut  ciðeri 
+Asmalýmescit/ Yakup 2 (Meyhane]  ............ ......... ...Yaprak  Ciðer
+Asmelýmescýt/ Dokuz Ece [Meyhane]... ......... ........  Þefin tavsiyesi  
+Asmalýmescit / Gedikli [ Meyhane).... .........  ............Hamsi  Güveç 
+Asmalýmescit/ Sisore Pide Evi  (Pideci).... ......... ..Trabzon peynirli  pide
+Asmalýmescit / Caným Ciðerim [Kehapcýl.... ........Ciðer kebap 
+Tepebasý / Mikla  [Lüks]....... ......... ..... ............ ...Marine Somon
+Tepebasý / Lokanta [Nu  Pera ve Nu Teras] (Lüks)....Fýrýnda  domatesli ve kalamarlý levrek 
+Kuledibi / Galataevi (Etnik)  ............ ......... ........  Çakapuli 
+Karaköy / Gullüoðlu  (Tatlýcý!..... ......... ......... ....... Fýstýklý Baklava
+Karaköy/Karakö y  Balýkçýsý (Balýkçýl ............ .......Kaðýtta Levrek 
+
+(Þiþli Niþantaþý,  Teþvikiye, Harbiye, Mecidiyeköy, Dolapdere  Çevreleri)  
+
+Elmadað /  Divan (Lüks] ............ ......... ......... ..Çýtýr levrek Fileto
+Elmadað / Divan  Pastanesi [Pastane] ............ .Forêt Noir  Pasta
+Harbiye / Boðaziçi Borsa [Türk  Mutfaðý]...... ..Kuzu  Tandýr 
+Harbiye / Loft Restoran Bar -Borsa [Lüksl ....  Pekin  Ördeði 
+Teþvfikiye/Carne  (Etnik)..... ......... ......... ......... ..Antrikot Rulo 
+Niþantaþý / Hünkar  (Esnaf Lokantasý]... ......... ...Kadýnbudu Köfte 
+Niþantaþý /  Tuus (Lüks)....... ......... ..........Domates soslu levrek  tava 
+Levent /Köþebaþý (Kebap]  ............ .........Tarsusi kebap 
+Teþvikiye /  Bahar Pastanesi (Pastane) .......Akyamaç  Turtasý 
+Teþvikiye/ Hacýbey Kebapçýsý (Kebapçý) ....  Bursa Kebabý 
+Maçka /  Chocolate Maçka (Lüks)....... ........Limon  soslu Piliç Izgara 
+Feriköþ /Hamov  (Etnik]..... ......... ......... ........Dalak  Dolmasý 
+Dolapdere /Apik (Çorbacý]  ............ ......... .... Ýþkembe 
+Kasýmpaþa / Geyikli /  (Corbacý) ............ ......Damardan  tuzlama 
+
+Beþiktaþ  Etiler. Ulus, Levent, Maslak Çevreleri! 
+
+Beyoðlu -  Kallavi [Meyhanel ............ ......... ......... .Çerkez Tavuðu
+Beyoðlu -  Lale iþkembecisi (Çorbacý) ............ ......Ýþkembe çorbasý 
+Etiler / Develi  (Kebap!   ............ ......... .... Çið köfte
+Etiler /  Günaydýn (Kebap) ............ ......... . Kilis Kebebý
+Ulus / Ulus 29 (Lüks)  ............ ......... .......Balkabaklý  Tortellini
+Ulus / Sunset Grill & Bar (Lüks) ........  Izgara Deniz Levreði
+Ulus /  Aþkana Mantý (Mantýcý) ........... Mantý 
+Levent / Konyalý Kanyon  (Lüks) ........ Keþkekli Kuzu  Ýncik
+Levent / Tike (Kebapçý) ............ .........Acýlý Tike Kebap
+Levent / Köþebaþý  (Kebapçý) ............ .. Tarsusi  Kebap 
+Levent / Köfteci Ramiz (Köfteci) ........  Ramiz köfte 
+Levent ve  Maslak / Hain Köfte (Köfteci) Hain  Köfte 
+Ýstinye Park/Günaydýn (Kebap) ............ ..Kilis Kebabý
+
+Boðaz,  Rumeli (Avrupa Yakasý) Ortaköy'den Sarýyer'e! 
+
+Ortaköy /  Feriye (Türk Mutfaðý) ..... Viþneli  Yapram Sarma 
+Bebek / Les Ambassadeurs (Lüks)… Levrek  Ambassador     
+Bebek / Meþhur Bebek  Badem Ezmesi (Tatlýcý] Badem  Ezmesi
+Emirgan / Changa (Lüks) ………… Tahin ve Miso Soslu Fýrýnda  Patlýcan
+Kuruçeþme / Park Fora (Balýk) ……………Pavella
+Kuruçeþme / Pafuli (Etnik)  ………………Mýhlama 
+Kuruçeþme /  Cafe di Dölce (Pastane) 342 …… Fettucine  Alfredo
+Kireçbumu/Tarihi Alý Baba (Balýk) ….. Kýlýç Þiþ 
+Tarabya / Kýyý (Balýk)  ………………………..Levrek  Buðulama
+Kýreçburnu/ Set Balýk (Balýk] ………………Kabaklý Hamsi 
+Kýrecburnu / Meþhur  Tarihi Kirecburnu Fýrýný (Hamuriþleri)……… Þekerli Kurabiye 
+Arnavutköy/Köfteci  Ali Baba (Kofteci) ………. Köfte
+Arnavutköy / Bahar Pastanesi  (Pastane) ……………..Akyamaç  Turtasý
+Yeniköy / Tarihi Yeniköy Börekçisi (Börekçi) ……….  Kuþüzümlü Kýymalý Börek  
+Emirgan / Muzedechanga (Lüks) …………. Tahin ve Miso Soslu Fýrýnda  Patlýcan  
+Sarýyer / Pide Ban (Pideci) ………………… Kýymalý Pide 
+Sarýyer ve Zekeriyaköy /  Tarihi Sarýyer Muhallebicisi (Pastane) …….. Tavukgöðsü 
+
+Tarihi  Yarýmada (Eminönü, Sirkeci, Sultanahmet, Kumkapý, Beyazýt, Süleymaniye, Haliç,  Fatih, Samatya, Yedikule, Edirnekapý Çevreleri)
+
+
+Sirkeci /  Konyalý (Esnaf tarzý)   …..Keþkekli Kuzu Ýncik 
+Sirkeci / Meþhur  Filibe Köftecisi (Köfteci)    ……….. Filibe Köftesi
+Sirkeci / Namlý Rumeli  Köftecisi (Köfteci) ………… Köfte  
+Eminönü / Adapazarý Islama Köftecisi (Köfteci)……. Islama Köfte 
+Eminönü / Hamdi  (Kebapçý] …………………. Fýstýklý  Kebap 
+Eminönü / Hacý Bekir (Tatlýcý] …………….. Özel tavsiye  yok
+Eminönü / Pandeli (Türk Mutfaðý) ………… Sebzeli Kuzu Ýncik 
+Nuruosmaniye /  Tarihi Subaþý (Esnaf) ………….. Süt Kuzu  Sarma 
+Kapalýçarþý / Havuzlu Lokanta (Esnaf) ………….. Beðendili Havuzlu Kebap 
+Sultanahmet /  Konyalý Topkapý Sarayý (Lüks] …… Keþkekli  Kuzu Ýncik
+Sultanahmet / Tarihi Sultanahmet Köftecisi (Köfteci]  ……. Köfte
+Sultanahmet /  Balýkçý Sabahattin (Balýkçý) Fener  balýðý  Kavurma 
+Kumkapý / Kör Agop (Meyhane) ………..  Kör Agop Usulü Balýk  Çorbasý
+Samatya / Develi (Kebapçý) ……. Çið Köfte 
+Samatya / Kuleli (Meyhane)  ……. Hamsili Pazý  Sarma
+Yedýkule / Safa (Meyhane) ………. Arnavut Ciðeri
+Süleymaniye /  Darüzziyafe (Türk Mutfaðý) …. Yufkalý  Darüzzürafe Köftesi 
+Fatih / Fatih Karadeniz Pidecisi  (Pideci) ……… Trabzon Peynirli  Pidesi
+Edirnekapý / Asitane (Türk Mutfaðý) …………. Kavun Dolmasý 
+Haliç / Tarihi Haliç  Ýþkembecisi [Çorbacý) ………. Zerde  
+
+
+(Bakýrköy,  Florya Ýkitelli Çevreleri) 
+
+
+ 
+ 
+
+Bakýrköy /  Chocolate (Lüks)  ……. Limon Soysu  Piliç Izgara 
+Bakýrköy / Hacý Bozanoðullarý (Kebap) ……..  Cevizli Kebap
+Bakýrköy / Lale  Ýþkembecisi (Çorbacý] ………….Ýþkembe  Çorbasý 
+Bakýrköy/ Köfteci Ramiz [Köfteci]…………….. Ramiz Köfte 
+Florya/Beyti (Et  Lokantasý)………………… Beyti Kebap ve  Döner
+Merter ve Ýkitelli / Fatih Karadeniz Pidecisi (Pideci) ……  Trabzon peynirli Pide  
+Yenisahra/ Doða Balýk (Balýk) …………… Palamut Pilaki 
+Beylikdüzü/ Kösebaþý  (Kebap) ……………. Tarsusi  Kebap
+
+
+ANADOLU  ASYA YAKASI
+
+ 
+
+Üsküdar  (Merkez, Altunizade, Ümraniye) 
+
+
+Üsküdar/  Kanaat Lokantasý [Esnaf] ………… Elbasan  Tava
+Kýsýklý-Altunýzade/ Mabeyin (Kebapçý) ………… Ekþili Taraklýk
+Ümraniye / Köfteci  Ramiz (Köfteci) ……………. Ramiz  Köfte
+
+
+Boðaz   Anadolu [Asya] Yakasý 
+
+
+Kuzguncuk  /Ýsmet Baha [Balýk] ………………..Patatesli  Börek 
+Çengelköy / Ýskele Çengelköy Restoran (Balýk) …. Deniz Mahsulleri Güveci (Atom)  
+Kanlýca / Körfez Restoran (Balýk) …………. Tuzda Balýk  (Levrek)
+
+Kadýköy  ve Çevresi  (Merkez, Bostancý, Küçükyalý, Alaþehir, Tuzla)  
+
+Kadýköy  Çarsýsý / Çiya (Türk Mutfaðý!...... .. Viþne  Kebabý
+Kadýköy Çarþýsý /Yanyalý Fehmi (Esnaf) ... ….. Acem Tavuðu
+Kadýköy Çarþýsý /Adapazarý  Islama Köftecisi (Köfteci) ............ . Islama Köfte
+Kadýköy Çarþýsý /Hacý  Bekir [Tatlýcý] ………….. Tavsiye Yok 
+Kadýköy/ Beyaz Fýrýn (Pastane] ………………  Makaron
+Moda Caddesi /Victor  Levi (Meyhane) …………. Victor Levi  Special
+Moda / Balýk - Moda Park Lokantasý (Balýk)   Hamsi Tava
+Acýbadem / Sahan  Vega (Kebap) …. Yeni Dünya  Kebabý 
+Kýzýltoprak / Coþkun Usta Bafra Pide ….Kapalý Peynirli Pide    
+Kalamýþ / Chocolate (Lüks] ……………….. Limon Soslu Piliç Izgara
+Kalamýþ /  Develi (Kebap ve Balýk) ……………… Çið  Köfte 
+Fenerbahçe / Kösebaþý (Kebap) ………….. Tarsusi Kebap 
+Göztepe / Aslý Börek  (Börek) ……….. Peynirli  Börek 
+Çiftehavuzlar/ Beyaz Fýrýn (Pastane) ………Makaron
+Erenköy ve Kalamýþ / Divan  Pastanesi (Pastane) ….. Foret noir  pasta
+Erenköy /  Beyaz Fýrýn (Pastane) ……………Makaron
+Caddebostan/ Sahan  (Kebap)  …………. Yenidünya  kebabý
+Bostancý / Hatay (Meyhane) …………. Humus
+Bostancý / Günaydýn Et Lokantasý  (Kebap ve Et) … Kilis  kebabý  
+Bostancý / Günaydýn Kasap (Kasap Kebap ve Et)  
+Bastancý / Sahan (Kebap)  …………. Yenidünya kebabý
+Kuçükyalý ve  Saþkýnbakkal / Foça Balýk (Balýk) ………. Foça  Lokma 
+Küçükyalý / Maria'nýn Bahçesi (Etnik) ……………  Selanik Usulü Midye  Dolmasý
+Küçükyalý / Lakerda Balýk (Balýk) ………….. Lakerda
+Suadiye / Köfteci Ramiz  (Köfteci) …………… Ramiz Köfte  
+Suadiye / Sahan (Kebap) ……….. Yenidünya  Kebabý
+Suadiye /  Beyaz Fýrýn (Pastane) ……… Makaron
+Sahrayýcedit / Günaydýn  (Kebap) ………. Kilis Kebabý   
+Maltepe/ Adapazarý Islama Köftecisi [Köfteci] ………  Islama Köfte 
+Atasehir /  DoðaBalýk (Balýkçý) ………….. Palamut  Pilaki
+Alaþehir / Sahan Vega [Türk Mutfaðý] ……… Yenidünya Kebabý
+Ataþehir/  Shorba Ýstanbul (Çorba) …………… Tom Kha Gai   
+Tuzla / Meraklý Köfteci (Köfteci) …………….. Köfte
+
+ADALAR  
+
+(Büyükada  ve Heybeliada)
+
+Büyükada /  Milto (Balýk! ……………. Kalamar  Tava
+Heybeliada / Mavi (Balýk)  ………….. Ýskorpit Sote
+
+HAVAALANI
+
+BTA
